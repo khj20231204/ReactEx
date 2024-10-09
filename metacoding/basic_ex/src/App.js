65a00ab25f50f1ecba5386105b735c8281cd2edc
khj20231204 <natista00@naver.com>
2024-10-09 13:07:01 +0900

    basic_ex

---
+++ metacoding/basic_ex/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react';
+
+function App() {
+  console.log("app 앱실행");
+
+  let sample = [
+    {id:1, name:"A"},
+    {id:2, name:"B"},
+    {id:3, name:"C"},
+    {id:4, name:"D"},
+  ];
+
+  let [users, setUsers] = useState(sample)
+  console.log("user:"+users);
+
+  let download = () => {
+    let sample2 = sample.concat({id:7, name:'kk'})
+    setUsers(sample2)
+  }
+
+  return (
+      
+    <div className="App">
+      <button onClick={download}>download</button>
+      {users.map((v)=> {
+       return(<h1>{v.name}</h1>)
+      })}
+    </div>
+  );
+}
+
+export default App;
