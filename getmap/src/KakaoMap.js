aeb1b5191c9651d0f26714330140107f9d8e3dab
khj20241204 <natista00@naver.com>
2024-10-11 11:11:21 +0900

    kakao_ignore

---
+++ getmap/src/KakaoMap.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const KakaoMap = () => {
+   return (
+      <div id="map" style={{width:'500px', heigth:'500px'}}>
+         
+      </div>
+   );
+};
+
+export default KakaoMap;
